153f665a3d93c4ffc58521e4a9859cab7a64f7e9
epicurus101 <mjrowbotham@gmail.com>
2022-03-07 22:45:02 +0000

    nearly loading a file

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
+async function loadFileAndPrintToConsole(url) {
+    try {
+      const response = await fetch(url);
+      const data = await response.text();
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  
+
+loadFileAndPrintToConsole('/wiki-100k.txt');
+
+
+
+const gameContainer = document.querySelector('.game-container')
+const wordList = ["one", "two", "three", "four", ]
+
+
 const buttonElement = document.createElement('button')
-buttonElement.textContent = key
-buttonElement.setAttribute('id', key)
+buttonElement.textContent = "hi friend!"
+buttonElement.setAttribute('id', "yolo")
 buttonElement.addEventListener('click', () => handleClick())
+gameContainer.appendChild(buttonElement)
+
+const textElement = document.createElement('div')
+textElement.textContent = ''
+gameContainer.appendChild(textElement)
 
 function handleClick(){
-
-    document.write('clicked')
+    console.log('clicked')
+    let ind = getRandomInt(wordList.length)
+    textElement.textContent = wordList[ind]
 }
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
